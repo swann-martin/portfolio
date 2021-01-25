c2e4967f0efc29969a423e0942024ea1aa203e94
Swann Martin <swannmartin1988@gmail.com>
2021-01-25 20:17:00 +0100

    ajoute le menu en mode mobile JS + nav css

--- dom.js
+++ dom.js
@@ -1,4 +1,6 @@
+const mainContainer = document.querySelector(".main-container");
 const nav__burger = document.querySelector(".nav__burger");
+const nav__burger__bar = document.querySelector(".nav__burger__bar");
 const nav__burger__bar__top = document.querySelector(".nav__burger__bar__top");
 const nav__burger__bar__middle = document.querySelector(
   ".nav__burger__bar__middle"
@@ -9,7 +11,20 @@ const nav__burger__bar__bottom = document.querySelector(
   ".nav__burger__bar__bottom"
 );
 
-nav__burger.addEventListener("click", () => {
-  h1.classList.toggle("red");
-  nav__burger.classList.toggle("nav__burger--open");
+const menu = document.querySelector(".menu");
+
+mainContainer.addEventListener("click", (evt) => {
+  if (
+    evt.target === nav__burger ||
+    evt.target === nav__burger__bar__top ||
+    evt === nav__burger__bar__middle ||
+    evt === nav__burger__bar__bottom ||
+    evt === nav__burger__bar
+  ) {
+    nav__burger.classList.toggle("nav__burger--open");
+    menu.classList.toggle("--invisible");
+  } else {
+    menu.classList.add("--invisible");
+    nav__burger.classList.remove("nav__burger--open");
+  }
 });
