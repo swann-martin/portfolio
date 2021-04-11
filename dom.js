4154a74d57979c57a1a96f3d28515e12b6b8b301
Swann Martin <swannmartin1988@gmail.com>
2021-04-11 18:55:19 +0200

    changed the font, the menu and the header

--- dom.js
+++ dom.js
@@ -1,30 +1,16 @@
 const mainContainer = document.querySelector(".main-container");
-const nav__burger = document.querySelector(".nav__burger");
-const nav__burger__bar = document.querySelector(".nav__burger__bar");
-const nav__burger__bar__top = document.querySelector(".nav__burger__bar__top");
-const nav__burger__bar__middle = document.querySelector(
-  ".nav__burger__bar__middle"
-);
 
-const h1 = document.querySelector("h1");
-const nav__burger__bar__bottom = document.querySelector(
-  ".nav__burger__bar__bottom"
-);
-
+const btnBurger = document.querySelector(".nav button");
+console.log(btnBurger);
 const menu = document.querySelector(".menu");
 
-mainContainer.addEventListener("click", (evt) => {
-  if (
-    evt.target === nav__burger ||
-    evt.target === nav__burger__bar__top ||
-    evt === nav__burger__bar__middle ||
-    evt === nav__burger__bar__bottom ||
-    evt === nav__burger__bar
-  ) {
-    nav__burger.classList.toggle("nav__burger--open");
-    menu.classList.toggle("--invisible");
-  } else {
+btnBurger.addEventListener("click", () => {
+  menu.classList.toggle("--invisible");
+});
+
+mainContainer.addEventListener("click", (e) => {
+  if (e.target !== btnBurger) {
+    console.log(e.target);
     menu.classList.add("--invisible");
-    nav__burger.classList.remove("nav__burger--open");
   }
 });
