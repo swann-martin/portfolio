6e5bcb0c22f37fb563dd62d900f0a1174feef83b
swann <swannmartin1988@gmail.com>
2023-03-14 10:54:18 +0100

    update react-router, remove app, router is in main

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Navigation from '../components/Navigation';
 import ProjectList from '../components/portfolio/ProjectList';
 
-const Portfolio = ({ data }: { data: any }) => {
+const Portfolio = () => {
   return (
     <div className="portfolio">
       <Navigation />
